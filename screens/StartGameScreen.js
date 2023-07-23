8685f57ab583ac61514c7ea9efcd6a31b3b8fcd7
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-23 14:31:38 +0800

    Adding a linear gradient

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -33,7 +33,7 @@ const sytles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
