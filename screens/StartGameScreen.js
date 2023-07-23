784ff663add94752b9d84f35b75dd8ae7fd58353
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-23 10:45:55 +0800

    configuration the textinput field to number

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={sytles.inputContainter}>
-      <TextInput style={sytles.numberInput} maxLength={2} />
+      <TextInput
+        style={sytles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,27 +20,27 @@ function StartGameScreen() {
 export default StartGameScreen;
 
 const sytles = StyleSheet.create({
-    inputContainter: {
-        marginTop: 100,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: '#72063c',
-        borderRadius: 8,
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.25
-    },
-    numberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: '#ddb52f',
-        borderBottomWidth: 2,
-        color: '#ddb52f',
-        marginVertical: 8,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    }
-})
+  inputContainter: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
