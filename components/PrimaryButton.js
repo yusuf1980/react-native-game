5833757fa581883c1f972b3623331ae139226f91
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-23 11:02:05 +0800

    visual feedback to the Buttons

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+  function pressHandlere() {
+    console.log("pressed");
+  }
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainter}>
+      <Pressable
+        style={styles.buttonInnerContainter}
+        onPress={pressHandlere}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainter: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+  buttonInnerContainter: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
