09b23390a325f392825e7446de2964f4b44d2cb4
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-24 10:34:54 +0800

    SafeAreaView on top

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,21 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function GameScreen() {
-    return <Text>Game Screen</Text>
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Highter or lower?</Text>
+      </View>
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24
+    }
+})
