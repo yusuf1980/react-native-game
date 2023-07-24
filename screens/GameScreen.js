b5e263e64a62d140df395979983c525809f711a7
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-24 11:27:00 +0800

    display random number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({userNumber}) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Highter or lower?</Text>
       </View>
@@ -15,8 +31,8 @@ function GameScreen() {
 export default GameScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 24
-    }
-})
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
