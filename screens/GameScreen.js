dc7358d4a18967fe8a85524f34a3d87361f1ec41
yusuf1980 <yusufachmad1980@gmail.com>
2023-07-25 11:02:44 +0800

    add icon (button icon)

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,9 +3,10 @@ import { View, StyleSheet, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
-import GameOverScreen from "./GameOverScreen";
+// import GameOverScreen from "./GameOverScreen";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+import {Ionicons} from "@expo/vector-icons"
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -63,12 +64,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
+              <Ionicons name="md-remove" size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-              +
+            <Ionicons name="md-add" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
